caf5dcb09799fca384053ec3c2ac50ca7b3e6174
Yeehaa <medialoperations@gmail.com>
2012-06-13 12:25:53 +0200

    4th

--- lib/github_hook.rb
+++ lib/github_hook.rb
@@ -4,7 +4,7 @@ require 'time'
 class GithubHook < Sinatra::Base
   def self.parse_git
     # Parse hash and date from the git log command.
-    sha1, date = 'git log HEAD~1..HEAD --pretty=format:%h^%ci'.strip.split('^')
+    sha1, date = `git log HEAD~1..HEAD --pretty=format:%h^%ci`.strip.split('^')
     set :commit_hash, sha1
     set :commit_date, Time.parse(date)
   end
@@ -20,7 +20,7 @@ class GithubHook < Sinatra::Base
     if settings.autopull?
       # Pipe stderr to stdout to make
       # sure we display everything
-      'git pull 2>&1'
+      `git pull 2>&1`
     else
       "ok"
     end
