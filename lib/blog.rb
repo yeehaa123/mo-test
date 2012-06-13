8350fc56cfafd412daa876b7e0a266118fb0b00b
Yeehaa <medialoperations@gmail.com>
2012-06-13 11:45:45 +0200

    Initial Commit

---
+++ lib/blog.rb
@@ -0,0 +1,51 @@
+require 'sinatra/base'
+require 'github_hook'
+require 'ostruct'
+require 'time'
+# require 'yaml'
+
+class Blog < Sinatra::Base
+  use GithubHook
+  
+  # File.expand_path generates an absolute path
+  # It also takes a path as second argument. The
+  # generated path is treated as being relative 
+  # to that path.
+  set :root, File.expand_path('../../', __FILE__)
+  set :articles, []
+  set :app_file, __FILE__
+  
+  # loop through all the article files
+  Dir.glob "#{root}/articles/*.md" do |file|
+    # parse meta data and content from file
+    meta, content     = File.read(file).split("\n\n", 2)
+
+    # generate a metadata object
+    article           = OpenStruct.new YAML.load(meta)
+    
+    # convert the date to a time object
+    article.date      = Time.parse article.date.to_s
+    
+    # add the content
+    article.content   = content
+    
+    # generate a slug for the url
+    article.slug    = File.basename(file, '.md')
+    
+    # set up the route
+    get "/#{article.slug}" do
+      erb :post, :locals => { :article => article }
+    end
+    
+    # Add article to list of articles
+    articles << article
+  end
+
+  # Sort articles by date, display new articles first
+  articles.sort_by! { |article| article.date }
+  articles.reverse!
+
+  get '/' do
+    erb :index
+  end
+end
---
+++ lib/github_hook.rb
@@ -0,0 +1,28 @@
+require 'sinatra/base'
+require 'time'
+
+class GithubHook < Sinatra::Base
+  def self.parse_git
+    # Parse hash and date from the git log command.
+    sha1, date = 'git log Head~1..Head --pretty=format:%h^%ci'.strip.split('^')
+    set :commit_hash, sha1
+    set :commit_date, Time.parse(date)
+  end
+  
+  set(:autopull) { production? }
+  parse_git
+  
+  post '/update' do
+    app.settings.reset!
+    load app.settings.app_file
+    
+    content_type :txt
+    if settings.autopull?
+      # Pipe stderr to stdout to make
+      # sure we display everything
+      'git pull 2>&1'
+    else
+      "ok"
+    end
+  end
+end
