842ff8ff5d423bd570e83e235a74b997327a8047
Yeehaa <medialoperations@gmail.com>
2012-06-13 12:01:24 +0200

    3nd

--- lib/github_hook.rb
+++ lib/github_hook.rb
@@ -4,7 +4,7 @@ require 'time'
 class GithubHook < Sinatra::Base
   def self.parse_git
     # Parse hash and date from the git log command.
-    sha1, date = 'git log Head~1..Head --pretty=format:%h^%ci'.strip.split('^')
+    sha1, date = 'git log HEAD~1..HEAD --pretty=format:%h^%ci'.strip.split('^')
     set :commit_hash, sha1
     set :commit_date, Time.parse(date)
   end
